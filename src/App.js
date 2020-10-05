feb8a59ce4c5a10553d0729f521d472f88496713
lhernandezr <luis.hernandez@ninjarmm.com>
2020-10-05 16:27:22 -0600

    intial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { increment, decrement } from './container/login/actions'
 
-function App() {
+function App({count, increaseCounter, decreaseCounter}) {
+  console.log(count)
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>1</p>
+        <button onClick={() => increaseCounter()}>Añadir</button>
+        <button onClick={() => decreaseCounter()}>Restar</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,4 +29,18 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    count: state.login,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCounter: () => dispatch(increment()),
+
+    decreaseCounter: () => dispatch(decrement()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
